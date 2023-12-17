604761e8f2b2c2778eec7e2fe36517fc5f8a4b9a
Christian Misael Prado Ciokler <72030877+chrisciokler@users.noreply.github.com>
2023-12-17 10:22:10 -0500

    adding supabase auth and database sql schema part 3

--- components/HeaderNav.tsx
+++ components/HeaderNav.tsx
@@ -2,15 +2,27 @@
 import { Button, Group } from '@mantine/core'
 import Link from 'next/link'
 import { useAuthStore } from './layouts/AppInitializer'
+import { api } from '@/apis'
+import { useToggle } from '@mantine/hooks'
 
 export const HeaderNav = () => {
   const auth = useAuthStore(state => state.auth);
+  const setAuth = useAuthStore(state => state.setAuth);
+  const [loading, toggle] = useToggle([false, true])
+
+  const signOutHandler = async () => {
+    toggle(true)
+    await api.auth.logout()
+    setAuth(null)
+    toggle(false)
+  }
 
   return (
     <Group>
       {auth ? (<div className='flex md:hidden gap-4'>
         <Link href="/docs" shallow ><Button variant='outline'>Docs</Button></Link>
         <Link href="/account" shallow ><Button variant='outline'>Account</Button></Link>
+        <Button variant='filled' color='red' loading={loading} disabled={loading} onClick={signOutHandler}>Sign out</Button>
       </div>) : (
         <Link href="/authenticate" shallow><Button variant="filled">Sign in</Button></Link>
       )
--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -4,8 +4,8 @@ import { HeaderNav } from '../HeaderNav';
 
 export const Header = () => (
   <header className='flex w-full items-center justify-center fixed z-50'>
-    <Box display="flex" className='w-full p-4 px-16 md:p-2 items-center justify-between z-50 shadow-xl' style={{ borderBottom: "1px solid rgb(37 38 43)", backdropFilter: 'blur(25px)' }}>
-      <Link href="/" shallow className='no-underline'><Title ml="md" order={3}>SupaTerms</Title></Link>
+    <Box display="flex" className='w-full p-4 px-16 md:p-4 items-center justify-between z-50 shadow-xl' style={{ borderBottom: "1px solid rgb(37 38 43)", backdropFilter: 'blur(25px)' }}>
+      <Link href="/" shallow className='no-underline'><Title order={3}>SupaTerms</Title></Link>
       <HeaderNav />
     </Box>
   </header>
